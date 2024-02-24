562cd5c9586e96753b7ed86aa39bd2f1d743ab7f
3ewv <115203266+3ewv@users.noreply.github.com>
2024-02-24 13:29:30 +0700

    Add files via upload
    register student => login => profile.student

--- Demo_layout/js/function.js
+++ Demo_layout/js/function.js
@@ -1,30 +1,28 @@
 
-import { laydsuser,add_data } from "./module.js";
-
-export async function form(University_Name,Company_Name,First_Name,representative_name,title_of_Representative,email,address,choose_a_password,confirm_password,Job){
-   add_data(University_Name,Company_Name,First_Name,representative_name,title_of_Representative,email,address,choose_a_password,confirm_password,Job);
-  
-   
-}
+import { laydsuser} from "./module.js";
 
 
 
-export async function check_user(User_Name, Email, Choose_a_password, Confirm_password, Job) {
+
+export async function check_user(First_Name, Email, Choose_a_password, Confirm_password, type) {
     let users =  await laydsuser();
   
-    users.forEach((element) => {
-      if (element.name == User_Name &&element.email == Email && element.choose_a_password == Choose_a_password &&element.confirm_password == Confirm_password &&element.type == Job) {
-        if (Job=="Student") {
-            sessionStorage.setItem("user", JSON.stringify(element));
-            window.location.href = `profile_student.html?id=${element.id}`;
-          }else if(Job=="University"){
-            sessionStorage.setItem("user", JSON.stringify(element));
-            window.location.href = `profile_university.html?id=${element.id}`;
-          }else{
-            sessionStorage.setItem("user", JSON.stringify(element));
-            window.location.href = `profile_employed.html?id=${element.id}`;
-          }
-      }
-    });
+   users.forEach((element) => {
+   
+    if (element.First_Name == First_Name && element.email == Email && element.choose_a_password == Choose_a_password && element.confirm_password == Confirm_password && element.type ==type) {
+      if (type=="student") {
+          sessionStorage.setItem("user", JSON.stringify(element));
+          window.location.href = `../html/profile/student.html?id=${element.id}`;
+        }else if(type=="university"){
+          sessionStorage.setItem("user", JSON.stringify(element));
+          window.location.href = `profile_university.html?id=${element.id}`;
+        }else{
+          sessionStorage.setItem("user", JSON.stringify(element));
+          window.location.href = `profile_employed.html?id=${element.id}`;
+        }
+
+    }
+  });
+   
   }
   
--- Demo_layout/js/module.js
+++ Demo_layout/js/module.js
@@ -5,27 +5,35 @@ export const laydsuser =()=>{
             .then(d=>d);
 }
 
-
-
-export const add_data = async (University_Name,Company_Name,First_Name,representative_name,title_of_Representative,email,address,choose_a_password,confirm_password,Job) =>{
-    const data_tk = await{
-        University_Name,Company_Name,First_Name,representative_name,title_of_Representative,email,address,choose_a_password,confirm_password,Job
-    }
+export const add_data = async (formDataObject) => {
+   
   
-    const update_data_tk = await{
+    const update_data_tk = {
         method: 'POST',
-        body: JSON.stringify(data_tk),
+        body: JSON.stringify(formDataObject),
         headers: {
             'Content-Type': 'application/json'
         }
-    }
-    console.log(update_data_tk);
-    return await fetch('http://localhost:3000/user', update_data_tk)
+    };
+
+    return fetch('http://localhost:3000/user', update_data_tk)
         .then(res => res.json())
-        .then(d => {
-            console.log(d);
-            window.location.href="demo_login.html";
-            return d;
-        });
+        .then(d => d);
+};
+
+export const update = async (object,id) => {
+    const update_data_tk =  await{
+        method: 'PUT',
+        body: JSON.stringify(object),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    };
+    return await fetch(`http://localhost:3000/user/${id}`, update_data_tk)
+    .then(res => res.json())
+    .then(d => d);
+
 
 }
+    
+    
